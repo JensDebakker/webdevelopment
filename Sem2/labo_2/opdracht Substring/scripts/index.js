ba1a4d5610b6526b739cbdedb9b0ffed289de28b
JessyIsCute <lord.vraxxer@gmail.com>
2025-02-14 18:44:11 +0100

    yes

--- Sem2/labo_2/opdracht Substring/scripts/index.js
+++ Sem2/labo_2/opdracht Substring/scripts/index.js
@@ -1,4 +1,21 @@
 const setup = () => {
-// deze code wordt pas uitgevoerd als de pagina volledig is ingeladen
+ //let btnSubstring = document.getElementById('substring');
+ //btnSubstring.addEventListener('click', calculate); why not work?
+
+
+    document.getElementById("calculate").addEventListener('click', calculate);
 }
+
+const calculate = () => {
+    const input = document.getElementById("input").value;
+    const arg1 = document.getElementById("para1").value;
+    const arg2 = document.getElementById("para2").value;
+    let output = document.getElementById("output");
+    const calculation = input.substring(arg1, arg2);
+
+    console.log(calculation);
+
+    document.getElementById("output").textContent = calculation;
+};
+
 window.addEventListener("load", setup);
